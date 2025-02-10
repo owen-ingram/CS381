22c989990aa8a661c32bc6eec4024749a1e3c2f6
owen-ingram <oingram@unr.edu>
2025-02-10 01:29:50 -0800

    commit as1.1

--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -3,40 +3,41 @@
 
 int main() {
     raylib::Window window(800, 400, "CS381 - Assignment 1");
-    InitAudioDevice();
-
+    raylib::AudioDevice audioDevice;
+    audioDevice.Init();
     GuiVolumeControlState volumeState = InitGuiVolumeControl();
+    raylib::Sound sfx("src/ping.wav");
+    raylib::Sound vocal("src/crowd.wav");
+    raylib::Music music("src/price-of-freedom.mp3");
 
-    Sound sfx = LoadSound("src/ping.wav");
-    Sound vocal = LoadSound("src/crowd.wav");
-    Music music = LoadMusicStream("src/price-of-freedom.mp3");
-
-    PlaySound(vocal);
-    PlayMusicStream(music);
+    vocal.Play();
+    music.Play();
 
     while (!window.ShouldClose()) {
         UpdateMusicStream(music);
-        SetSoundVolume(sfx, volumeState.SFXSliderValue / 100.0f);
-        SetSoundVolume(vocal, volumeState.DialogueSliderValue / 100.0f);
-        SetMusicVolume(music, volumeState.MusicSliderValue / 100.0f);
+        sfx.SetVolume(volumeState.SFXSliderValue / 100.0f);
+        vocal.SetVolume(volumeState.DialogueSliderValue / 100.0f);
+        music.SetVolume(volumeState.MusicSliderValue / 100.0f);
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
         GuiVolumeControl(&volumeState);
+
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), {24, 304, 256, 24})) {
             PingButton();
         }
         EndDrawing();
     }
-    UnloadSound(sfx);
-    UnloadSound(vocal);
-    UnloadMusicStream(music);
-    CloseAudioDevice();
+
+    sfx.Unload();
+    vocal.Unload();
+    music.Unload();
+    audioDevice.Close();
     CloseWindow();
-    
     return 0;
 }
 
 void PingButton() {
-    static Sound sfx = LoadSound("src/ping.wav");
-    PlaySound(sfx);
+    static raylib::Sound sfx("src/ping.wav");
+    sfx.Play();
 }
